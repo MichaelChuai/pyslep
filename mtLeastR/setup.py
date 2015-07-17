13af9627094bf2e98e04ee8cbffb685f40f3df65
Michael Chuai <18alexanderm117@tongji.edu.cn>
2015-07-18 01:26:42 +0800

    Moved setup.py

--- mtLeastR/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-from numpy import get_include
-
-ext_main = Extension('mtLeastR', ['mtLeastR.pyx', 'ep21R.c', 'eppMatrix.c', 'epph.c'], include_dirs=['.', get_include()])
-
-setup(ext_modules=cythonize([ext_main]))
